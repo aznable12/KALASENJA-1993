feaf322734944d76d3b6072a4549b1eb239f6538
aznable12 <callmeiwang@gmail.com>
2021-11-29 15:35:56 +0700

    edit

--- KALASENJA-1993/Assets/MainMenu.cs
+++ KALASENJA-1993/Assets/MainMenu.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);// this can load the scene number 1 (game)//SceneManager.GetActiveScene().buildIndex +1
+
+    }
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
 }
